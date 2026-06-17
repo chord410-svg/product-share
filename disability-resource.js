d05d558826dd79772b1c631329ce945c6608391c
chord410-svg <chord410@gmail.com>
2026-06-17 19:57:14 +0800

    deploy resource nav asset disability-resource.js

--- disability-resource.js
+++ disability-resource.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260617-detail-fix-v2';
+const CACHE_VERSION = '20260617-detail-fix-v4';
 const PACKAGE_STORAGE_KEY = 'disability_knowledge_packages_v1';
 const KNOWLEDGE_PACK_SCHEMA_VERSION = 'knowledgepack.v1';
 const KNOWLEDGE_PACK_MANIFEST_MARKER = 'KNOWLEDGE_PACK_MANIFEST';
